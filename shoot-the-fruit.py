00440732db13c7be0728d026201285f63a66150e
Leonardo Hernandez <ninihernandez@hotmail.com>
2022-04-14 20:19:36 -0500

    Add files via upload
    This game was created in Python3 using the IDLE shell and editor.

---
+++ shoot-the-fruit.py
@@ -0,0 +1,43 @@
+import pgzrun
+from random import randint
+
+apple = Actor("apple")
+orange = Actor("orange")
+pineapple = Actor("pineapple")
+
+def draw():
+    screen.clear()
+    apple.draw()
+    orange.draw()
+    pineapple.draw()
+
+def place_apple():
+    apple.x = randint(10, 800)
+    apple.y = randint(10, 600)
+
+def place_orange():
+    orange.x = randint(10, 800)
+    orange.y = randint(10, 600)
+
+def place_pineapple():
+    pineapple.x = randint(10, 800)
+    pineapple.y = randint(10, 600)
+
+count = 0
+
+def on_mouse_down(pos):
+    global count
+    if apple.collidepoint(pos):
+        count += 1
+        print("Good shot! Score = ", count)
+        place_apple()
+        place_orange()
+        place_pineapple()
+    else:
+        print("You missed! Score = ", count)
+
+place_apple()
+place_orange()
+place_pineapple()
+
+pgzrun.go()
